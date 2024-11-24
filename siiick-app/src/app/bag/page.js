9a92fdf8f15f4373c2abc7746ee032252387bb8f
Matthew Garza <matt.garza34@gmail.com>
2024-11-24 15:15:43 -0600

    fixed subtotal on bag page

--- siiick-app/src/app/bag/page.js
+++ siiick-app/src/app/bag/page.js
@@ -39,7 +39,7 @@ export default function Page(){
     const { bagItems, addToBag, removeFromBag, decrementQuantity, subTotal, itemsCount } =  useContext(ShoppingBagContext);
     const [validCode, setValidCode] = useState(false);
     const [promoCode, setPromoCode] = useState('');
-    const [subTotal, setSubtotal] = useState(0);
+   // const [subTotal, setSubtotal] = useState(0);
     const validCodeExp = /siiick15/;
     const isValidCode = (e) => {validCodeExp.test(e.target.value) ? setValidCode(true) : setValidCode(false)}
 
